3eba975365178a8dee45193052027467a377ba5d
barack <barackannan00@gmail.com>
2026-08-16 13:11:00 +0300

    Add plant shop functionality

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -3,12 +3,12 @@ import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage() {
+function PlantPage({ plants, addPlant, setSearchTerm }) {
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addPlant={addPlant} />
+      <Search setSearchTerm={setSearchTerm} />
+      <PlantList plants={plants} />
     </main>
   );
 }
