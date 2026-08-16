dc5ff1b39d85d762b884e3eb8b6a5be2ac8f3f8e
barack <barackannan00@gmail.com>
2026-08-16 11:28:36 +0300

    refactor: update App component to manage plant state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  const [plants, setPlants] = useState([]);
   return (
     <div className="app">
       <Header />
